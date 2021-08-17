e44c02c5c22f96eda72d303c31a1a03be9afee72
Teppei-tt <77786751+Teppei-tt@users.noreply.github.com>
2021-08-17 20:41:13 +0900

    TypeScriptによる型定義をせず、バクを起こしたアプリの例

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  // res=response
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchData}>データ取得する</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
