91955776a97b978961003e4e47f05ab0354dd895
Teppei-tt <77786751+Teppei-tt@users.noreply.github.com>
2021-08-17 21:52:41 +0900

    stateへの型定義

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,31 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 
+// 一般的にTypeは大文字で始めると良い
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   // res=response
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得する</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userid} />
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
