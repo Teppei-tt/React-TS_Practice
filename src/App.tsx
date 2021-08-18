bb719e293b78e3a7e5267629a48158c3b7af56e3
Teppei-tt <77786751+Teppei-tt@users.noreply.github.com>
2021-08-18 23:02:05 +0900

    Propsの型定義

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,11 @@ export default function App() {
     <div className="App">
       <button onClick={onClickFetchData}>データ取得する</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userId} />
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/Todo.jsx
+++
@@ -1,4 +0,0 @@
-export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p>{`${title}(ユーザー${userid})`}</p>;
-};
